8a152d975c0bbbf8bdba417273e933a40e070c81
v7Aquaman <gezsef@gmail.com>
2021-07-13 09:58:49 +0200

    Splitter opp stringen i nye variabler og verdier

--- FBU_SurveyScript.R
+++ FBU_SurveyScript.R
@@ -18,20 +18,31 @@ df <- fromJSON("C:/Users/gse/Documents/R/FBU_SurveyData/questionnaireData.json",
 # kodet som strings og maa splittes og plasseres i nye variabler. 
 
 
+# c("hus1_4 == 4", "bol1_1 == 4", "bol2_1 == 120"
+
+# rydder litt opp for jeg splitter opp mellom variabelnavn og verdeiene.
+df$answers <- gsub("c\\(", "", df$answers)
+
+
+
+do.call("<-",df$answers(parameter_name, parameter_value))
+
+
+df %>%
+  separate_rows(answers, sep = ",\\s+") %>%
+  separate(answers, into = c("key", "value"), sep="=\\s+") %>%
+  mutate(key = na_if(key, "No Data")) %>%
+  pivot_wider(names_from = key, values_from = value) %>%
+  #  select(-`NA`) %>% 
+  view()
 
 
 
 
-
-
-
-
-
-
-
-
-
-
+data.frame(do.call("cbind", strsplit(as.character(df$answers), "==",
+                                     fixed = TRUE
+                                     ))) %>% 
+  view()
 
 
 
