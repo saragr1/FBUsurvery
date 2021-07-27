37fd5cbc6386c6d0e3ad7870703a15066652f86b
saragr1 <87315750+saragr1@users.noreply.github.com>
2021-07-27 15:53:12 +0200

    Add files via upload

--- utpakkingavdata.R
+++ utpakkingavdata.R
@@ -52,6 +52,7 @@ df1 <- as.data.frame(df1)
 df1[df1 == "NULL"] <- NA_character_
 sum(is.na(df1)) # 42058, tyder på at det er blitt NAs
 
+
 #### Fjerner () , "" i c("3", "4000")
 
 df2 <- df1[,-1] %>%
@@ -63,20 +64,96 @@ df2 <- as.data.frame(cbind(df$respondentId, df2))
 # Endrer navn på respondentId variabelen
 names(df2)[names(df2) == "df$respondentId"] <- "respondentId"
 
+
+#### Fjerner c og to verdier i én kolonne i enkelte variabler ####
+
+## Varigmøbler1_3 (fremgangsmåte)
+# Separerer varigmøbler1_3 (c3 10000) inn i to nye variabler 
+df2 <- df2 %>%
+  separate(varigmøbler1_3, c("varigmøbler1_3_n", "varigmøbler2_3_n"))
+# Slår sammen verdiene i varigmøbler2_3_n og 2_3 
+df2$varigmøbler2_3_n <- coalesce(df2$varigmøbler2_3_n, df2$varigmøbler2_3)
+# Fjerner varigmøbler2_3
+df2 <- subset(df2, select = -varigmøbler2_3) ### OBS KAN GJØRES FOR ALLE?? ###
+# Fjerner c i varigmøbler1_3_n
+df2$varigmøbler1_3_n <- gsub("c", "", df2$varigmøbler1_3_n)
+
+## Varigmøbler1_4
+df2 <- df2 %>%
+  separate(varigmøbler1_4, c("varigmøbler1_4_n", "varigmøbler2_4_n")) 
+df2$varigmøbler2_4_n <- coalesce(df2$varigmøbler2_4_n, df2$varigmøbler2_4)
+df2 <- subset(df2, select = -varigmøbler2_4)
+df2$varigmøbler1_4_n <- gsub("c", "", df2$varigmøbler1_4_n)
+
+## Varigmøbler1_6
+df2 <- df2 %>%
+  separate(varigmøbler1_6, c("varigmøbler1_6_n", "varigmøbler2_6_n"))
+df2$varigmøbler2_6_n <- coalesce(df2$varigmøbler2_6_n, df2$varigmøbler2_6)
+df2 <- subset(df2, select = -varigmøbler2_6)
+df2$varigmøbler1_6_n <- gsub("c", "", df2$varigmøbler1_6_n)
+
+## Varigmøbler1_2
+df2 <- df2 %>%
+  separate(varigmøbler1_2, c("varigmøbler1_2_n", "varigmøbler2_2_n"))
+df2$varigmøbler2_2_n <- coalesce(df2$varigmøbler2_2_n, df2$varigmøbler2_2)
+df2 <- subset(df2, select = -varigmøbler2_2)
+df2$varigmøbler1_2_n <- gsub("c", "", df2$varigmøbler1_2_n)
+
+## Varigmøbler1_7
+df2 <- df2 %>%
+  separate(varigmøbler1_7, c("varigmøbler1_7_n", "varigmøbler2_7_n"))
+df2$varigmøbler2_7_n <- coalesce(df2$varigmøbler2_7_n, df2$varigmøbler2_7)
+df2 <- subset(df2, select = -varigmøbler2_7)
+df2$varigmøbler1_7_n <- gsub("c", "", df2$varigmøbler1_7_n)
+
+## Ved3_3
+df2 <- df2 %>%
+  separate(ved3_3, c("ved3_3_n", "ved4_3_n"))
+
+## Varigmøbler1_5
+df2 <- df2 %>%
+  separate(varigmøbler1_5, c("varigmøbler1_5_n", "varigmøbler2_5_n"))
+df2$varigmøbler2_5_n <- coalesce(df2$varigmøbler2_5_n, df2$varigmøbler2_5)
+df2 <- subset(df2, select = -varigmøbler2_5)
+df2$varigmøbler1_5_n <- gsub("c", "", df2$varigmøbler1_5_n)
+
+## Varigmøbler1_8
+df2 <- df2 %>%
+  separate(varigmøbler1_8, c("varigmøbler1_8_n", "varigmøbler2_8_n"))
+df2$varigmøbler2_8_n <- coalesce(df2$varigmøbler2_8_n, df2$varigmøbler2_8)
+df2 <- subset(df2, select = -varigmøbler2_8)
+df2$varigmøbler1_8_n <- gsub("c", "", df2$varigmøbler1_8_n)
+
+## Varigmøbler1_1
+df2 <- df2 %>%
+  separate(varigmøbler1_1, c("varigmøbler1_1_n", "varigmøbler2_1_n"))
+df2$varigmøbler2_1_n <- coalesce(df2$varigmøbler2_1_n, df2$varigmøbler2_1)
+df2 <- subset(df2, select = -varigmøbler2_1)
+df2$varigmøbler1_1_n <- gsub("c", "", df2$varigmøbler1_1_n)
+
+## Ved3_2
+df2 <- df2 %>%
+  separate(ved3_2, c("ved3_2_n", "ved4_2_n"))
+
+## Leiefritid_1
+df2 <- df2 %>%
+  separate(leiefritid_1, c("leiefritid_1_n", "leiefritid_12_n"))
+
+
+
 ####  ####
 gsub("[[:punct:]]", "", df2)
 df3 <- as.data.frame(gsub("[[:punct:]]", "", as.matrix(df2)))
 
-class(df3$bol2_1)
-#as.numeric(df3$bol2_1 <- )
+
 
 #### Gjøremål ####
 # 1. Gjøre IOnumber til numeric |JA
 # 2. Fjerne NULL og erstatte med NA |JA
 # 3. Fjerne ) etter verdier i noen variabler i datasettet |JA
-# 4. Sjekke om det er noen verdier (ekstreme verdier), 2 standardavvik fra gjennomsnittet (matrise for å sjekke det) |
+# 4. Fjerne c og at det er to verdier i én celle | PÅGÅENDE
+# 5. Gjøre alle tallvariabler om til numeric 
 
 
 
-
-
+           
